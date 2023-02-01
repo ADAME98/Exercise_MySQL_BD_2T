f7d0985c32a48b00332adea810780ca33cc37cc5
ADAME98 <114871837+ADAME98@users.noreply.github.com>
2023-02-01 10:41:52 +0100

    Add files via upload

--- Ejercicio12.sql
+++ Ejercicio12.sql
@@ -16,7 +16,8 @@ CREATE TABLE coches(
 matriculaCoche varchar (8),
 marcaCoche varchar (45),
 colorCoche varchar (45),
-modeloCoche varchar (45)
+modeloCoche varchar (45),
+Primary key (matriculaCoche)
 );
 
 CREATE TABLE historico(
@@ -27,5 +28,34 @@ fechaDesde date,
 fechaHasta date,
 Primary Key (idHistorico),
 Foreign key (matriculaCocheFK)
-		references (),
+		references coches(matriculaCoche),
+Foreign key (dniPersonaFK)
+		references personas(dniPersona)
 );
+
+SELECT*FROM personas;
+
+INSERT INTO `ejercicio12`.`personas` (`dniPersona`, `nombrePersona`, `ApellidoPersona`, `direccionPersona`) VALUES ('47569847H', 'Manuel', 'Ramirez', 'C/Sahara');
+INSERT INTO `ejercicio12`.`personas` (`dniPersona`, `nombrePersona`, `ApellidoPersona`, `direccionPersona`) VALUES ('25484587T', 'Ismael', 'Tunez', 'C/Velazquez');
+
+INSERT INTO personas value(
+'12345678F','Jaime','Gomez','C/San Dima'
+
+);
+INSERT INTO personas value(
+'12345678E','Lucia','Gonzalez','C/San Jorge'
+);
+
+SELECT*FROM coches;
+
+INSERT INTO `ejercicio12`.`coches` (`matriculaCoche`, `marcaCoche`, `colorCoche`, `modeloCoche`) VALUES ('4565KCB', 'Opel', 'Azul', 'Astra');
+INSERT INTO `ejercicio12`.`coches` (`matriculaCoche`, `marcaCoche`, `colorCoche`, `modeloCoche`) VALUES ('1587GBV', 'Kia', 'Blanco', 'Ceed');
+
+INSERT INTO coches value(
+'9865DCJ','Ferrari','Rojo','laFerrari'
+);
+INSERT INTO coches value(
+'7894FHD','Porche','Verde','Panamera'
+);
+
+SELECT*FROM historico;
